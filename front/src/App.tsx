33be3a08f26444ee315175e7f5e449f75b517f8a
nesyG <iness.gvoic@gmail.com>
2022-12-08 20:12:00 +0100

    initial commit,config set up

---
+++ front/src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent } from "react";
+
+const App: FunctionComponent = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there, I am react, typescript and webpack!</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
